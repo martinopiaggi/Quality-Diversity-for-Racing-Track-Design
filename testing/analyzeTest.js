77c9fceda5f383d122b87e7de76979ad1ac9cad9
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-05-29 18:08:41 +0200

    better analyze data with std deviation

--- testing/analyzeTest.js
+++ testing/analyzeTest.js
@@ -12,6 +12,8 @@ async function collectData() {
         let totalDeltaY = 0;
         let totalLength = 0;
         let count = 0;
+        let sumOfSquaresDeltaX = 0;
+        let sumOfSquaresDeltaY = 0;
 
         for (const file of jsonFiles) {
             const filePath = path.join(JSON_DIR, file);
@@ -27,10 +29,26 @@ async function collectData() {
         const averageDeltaY = totalDeltaY / count;
         const averageLength = totalLength / count;
 
+        for (const file of jsonFiles) {
+            const filePath = path.join(JSON_DIR, file);
+            const data = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+
+            sumOfSquaresDeltaX += Math.pow(data.deltaX - averageDeltaX, 2);
+            sumOfSquaresDeltaY += Math.pow(data.deltaY - averageDeltaY, 2);
+        }
+
+        const varianceDeltaX = Math.sqrt(sumOfSquaresDeltaX / (count - 1)) ;
+        const varianceDeltaY = Math.sqrt(sumOfSquaresDeltaY / (count - 1)) ;
+
         console.log(`Number of JSON files: ${count}`);
-        console.log(`Average deltaX: ${averageDeltaX}`);
-        console.log(`Average deltaY: ${averageDeltaY}`);
         console.log(`Average length: ${averageLength}`);
+        console.log(`Average deltaX: ${averageDeltaX}`);
+        
+        console.log(`Average deltaY: ${averageDeltaY}`);
+        console.log(`Variance deltaX: ${varianceDeltaX}`);
+        console.log(`Variance deltaY: ${varianceDeltaY}`);
+        
+        
     } catch (err) {
         console.error(`Error: ${err.message}`);
     }
