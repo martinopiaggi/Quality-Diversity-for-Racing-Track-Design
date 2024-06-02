7fe06e8798e4fbaad75be83c7d5f8fe4ea384934
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-02 18:58:24 +0200

    Angle degrees in output

--- testing/analyzeTest.js
+++ testing/analyzeTest.js
@@ -11,9 +11,11 @@ async function collectData() {
         let totalDeltaX = 0;
         let totalDeltaY = 0;
         let totalLength = 0;
+        let totalDeltaAngleDegrees = 0;
         let count = 0;
         let sumOfSquaresDeltaX = 0;
         let sumOfSquaresDeltaY = 0;
+        let sumOfSquaresDeltaAngleDegrees = 0;
 
         for (const file of jsonFiles) {
             const filePath = path.join(JSON_DIR, file);
@@ -22,12 +24,14 @@ async function collectData() {
             totalDeltaX += data.deltaX;
             totalDeltaY += data.deltaY;
             totalLength += data.length;
+            totalDeltaAngleDegrees += data.deltaAngleDegrees;
             count++;
         }
 
         const averageDeltaX = totalDeltaX / count;
         const averageDeltaY = totalDeltaY / count;
         const averageLength = totalLength / count;
+        const averageDeltaAngleDegrees = totalDeltaAngleDegrees / count;
 
         for (const file of jsonFiles) {
             const filePath = path.join(JSON_DIR, file);
@@ -35,20 +39,22 @@ async function collectData() {
 
             sumOfSquaresDeltaX += Math.pow(data.deltaX - averageDeltaX, 2);
             sumOfSquaresDeltaY += Math.pow(data.deltaY - averageDeltaY, 2);
+            sumOfSquaresDeltaAngleDegrees += Math.pow(data.deltaAngleDegrees - averageDeltaAngleDegrees, 2);
         }
 
-        const varianceDeltaX = Math.sqrt(sumOfSquaresDeltaX / (count - 1)) ;
-        const varianceDeltaY = Math.sqrt(sumOfSquaresDeltaY / (count - 1)) ;
+        const varianceDeltaX = Math.sqrt(sumOfSquaresDeltaX / (count - 1));
+        const varianceDeltaY = Math.sqrt(sumOfSquaresDeltaY / (count - 1));
+        const varianceDeltaAngleDegrees = Math.sqrt(sumOfSquaresDeltaAngleDegrees / (count - 1));
 
         console.log(`Number of JSON files: ${count}`);
         console.log(`Average length: ${averageLength}`);
         console.log(`Average deltaX: ${averageDeltaX}`);
-        
         console.log(`Average deltaY: ${averageDeltaY}`);
+        console.log(`Average deltaAngleDegrees: ${averageDeltaAngleDegrees}`);
         console.log(`Variance deltaX: ${varianceDeltaX}`);
         console.log(`Variance deltaY: ${varianceDeltaY}`);
-        
-        
+        console.log(`Variance deltaAngleDegrees: ${varianceDeltaAngleDegrees}`);
+
     } catch (err) {
         console.error(`Error: ${err.message}`);
     }
