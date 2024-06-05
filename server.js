1be1bc453ef5961b5b9a7e78c5b0a0c5ecbb7c6c
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-05 22:42:11 +0200

    Deploy still doesn't show tracks

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ import { promises as fs } from 'fs';
 
 const app = express();
 const __dirname = path.resolve();
-const BASE_DIR = __dirname;
+const TESTS_DIR = path.join(__dirname, 'testing/tests');
 
 // Middleware to serve static files and directory listing
-app.use(express.static(BASE_DIR));
-app.use(serveIndex(BASE_DIR, { icons: true }));
+app.use(express.static(__dirname));
+app.use('/testing/tests', express.static(TESTS_DIR), serveIndex(TESTS_DIR, { icons: true }));
 
 // Serve the gallery.html file on the root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(BASE_DIR, 'gallery.html'));
+  res.sendFile(path.join(__dirname, 'gallery.html'));
 });
 
 app.get('/load-track-data', async (req, res) => {
   try {
-    const TESTS_DIR = path.join(BASE_DIR, 'testing/tests');
     const files = (await fs.readdir(TESTS_DIR)).filter(file => file.endsWith('.json'));
     const trackDataPromises = files.map(async file => {
       const data = await fs.readFile(path.join(TESTS_DIR, file), 'utf-8');
