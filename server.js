498b44a9ef88d6703bf8090cf9edd286ea133dbb
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-05 22:01:43 +0200

    added all tests for visualization purposes

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ import { promises as fs } from 'fs';
 
 const app = express();
 const __dirname = path.resolve();
-const TESTS_DIR = path.join(__dirname, 'testing/tests');
+const BASE_DIR = __dirname;
 
 // Middleware to serve static files and directory listing
-app.use(express.static(__dirname));
-app.use(serveIndex(__dirname, { icons: true }));
+app.use(express.static(BASE_DIR));
+app.use(serveIndex(BASE_DIR, { icons: true }));
 
-// Serve the track_visualizer.html file on the root route
+// Serve the gallery.html file on the root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'track_visualizer.html'));
+  res.sendFile(path.join(BASE_DIR, 'gallery.html'));
 });
 
 app.get('/load-track-data', async (req, res) => {
   try {
+    const TESTS_DIR = path.join(BASE_DIR, 'testing/tests');
     const files = (await fs.readdir(TESTS_DIR)).filter(file => file.endsWith('.json'));
     const trackDataPromises = files.map(async file => {
       const data = await fs.readFile(path.join(TESTS_DIR, file), 'utf-8');
