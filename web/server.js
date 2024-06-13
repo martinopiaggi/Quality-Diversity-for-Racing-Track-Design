3b8783c7d8b2249b6de1fe450d69f13f10885790
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-13 22:30:32 +0200

    refactor for website deploy

--- web/server.js
+++ web/server.js
@@ -1,20 +1,56 @@
 import express from 'express';
 import serveIndex from 'serve-index';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import { promises as fs } from 'fs';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const __dirname = path.resolve();
-const TESTS_DIR = path.join(__dirname, '..', 'data', 'tests');
+const PUBLIC_DIR = path.join(__dirname, 'public');
+const TESTS_DIR = path.join(PUBLIC_DIR, 'data', 'tests');
 
-// Middleware to serve static files and directory listing
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/data/tests', express.static(TESTS_DIR), serveIndex(TESTS_DIR, { icons: true }));
+// Custom template for directory listings
+const template = `
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8">
+    <title><%- title %></title>
+    <style>
+      body {
+        font-family: Arial, sans-serif;
+      }
+      ul {
+        list-style: none;
+        padding: 0;
+      }
+      li {
+        margin-bottom: 10px;
+      }
+      a {
+        text-decoration: none;
+        color: #333;
+      }
+    </style>
+  </head>
+  <body>
+    <h1><%- title %></h1>
+    <ul>
+      <% files.forEach(function(file) { %>
+        <% if (file.name !== 'index.html') { %>
+          <li><a href="<%= file.href %>"><%= file.name %></a></li>
+        <% } %>
+      <% }); %>
+    </ul>
+  </body>
+  </html>
+`;
 
-// Serve the gallery.html file on the root route
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Middleware to serve static files and directory listing from the public directory
+app.use(express.static(PUBLIC_DIR));
+app.use('/data/tests', express.static(TESTS_DIR), serveIndex(TESTS_DIR, { template, icons: true }));
 
 app.get('/load-track-data', async (req, res) => {
   try {
@@ -31,7 +67,11 @@ app.get('/load-track-data', async (req, res) => {
   }
 });
 
+// Catch-all route to serve index.html for any unmatched routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(PUBLIC_DIR, 'index.html'));
+});
+
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
-
