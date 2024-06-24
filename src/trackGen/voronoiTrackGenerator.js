fffd20fb2e4d7d045876783c9737f7c0d1bd3ead
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-06-24 19:00:13 +0200

    Voronoi crossover DisjointSets + BFS heuristic implemented

--- src/trackGen/voronoiTrackGenerator.js
+++ src/trackGen/voronoiTrackGenerator.js
@@ -8,6 +8,7 @@ export class VoronoiTrackGenerator {
         this.dataSet = dataSet.length > 0 ? [...dataSet,...selectedVoronoiSites] : this.generatePoints();
         this.voronoi = new Voronoi();
         this.diagram = this.voronoi.compute(this.dataSet, this.bbox);
+        this.patchPath = [];
         this.selectedCells = selectedVoronoiSites.length > 0 ?  
             this.sitesFromInput(selectedVoronoiSites) : this.selectCellsForTrack(size);
         this.trackEdges = this.findTrackEdges();
@@ -33,8 +34,94 @@ export class VoronoiTrackGenerator {
                 selectedCells.push(cell);
             }
         });
+
+        // Check if cells are connected and connect them if not
+        this.ensureConnectedCells(selectedCells);
+
         return selectedCells;
     }
+
+    ensureConnectedCells(selectedCells) {
+        const disjointSet = new DisjointSet(selectedCells);
+
+        // Union adjacent cells
+        for (let cell of selectedCells) {
+            const neighbors = this.getNeighborCells(cell);
+            for (let neighbor of neighbors) {
+                if (selectedCells.includes(neighbor)) {
+                    disjointSet.union(cell, neighbor);
+                }
+            }
+        }
+
+        // Find disconnected components
+        const components = new Map();
+        for (let cell of selectedCells) {
+            const root = disjointSet.find(cell);
+            if (!components.has(root)) {
+                components.set(root, []);
+            }
+            components.get(root).push(cell);
+        }
+
+        // If more than one component, connect them
+        console.log(components.size)
+        if (components.size > 1) {
+            const componentRoots = Array.from(components.keys());
+            for (let i = 1; i < componentRoots.length; i++) {
+                const path = this.findShortestPath(componentRoots[i-1], componentRoots[i]);
+                if (path) {
+                    for (let cell of path) {
+                        //if not present add it
+                        if (!selectedCells.some(c => this.comparePoints(c.site, cell.site))) {
+                            selectedCells.push(cell); //add necessary cells to selectedCells
+                            this.patchPath.push(cell); //this is just for visualization/debug purposes
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    getNeighborCells(cell) {
+        const neighbors = [];
+        for (let halfedge of cell.halfedges) {
+            const neighborSite = halfedge.edge.lSite !== cell.site ? halfedge.edge.lSite : halfedge.edge.rSite;
+            if (neighborSite) {
+                neighbors.push(this.diagram.cells[neighborSite.voronoiId]);
+            }
+        }
+        return neighbors;
+    }
+
+    findShortestPath(startCell, endCell) {
+        const queue = [[startCell]];
+        const visited = new Set();
+
+        while (queue.length > 0) {
+            const path = queue.shift();
+            const currentCell = path[path.length - 1];
+
+            if (currentCell === endCell) {
+                return path;
+            }
+
+            if (!visited.has(currentCell)) {
+                visited.add(currentCell);
+                const neighbors = this.getNeighborCells(currentCell);
+                for (const neighbor of neighbors) {
+                    if (!visited.has(neighbor)) {
+                        queue.push([...path, neighbor]);
+                    }
+                }
+            }
+        }
+
+        return null; // No path found
+    }
+
+    
+
     
     findCellByCoordinates(x, y) {
         for (let cell of this.diagram.cells) {
@@ -118,5 +205,35 @@ export class VoronoiTrackGenerator {
 
     comparePoints(point1, point2) {
         return point1.x === point2.x && point1.y === point2.y;
+    }
+}
+
+
+class DisjointSet {
+    constructor(elements) {
+        this.parent = new Map(elements.map(e => [e, e]));
+        this.rank = new Map(elements.map(e => [e, 0]));
+    }
+
+    find(element) {
+        if (this.parent.get(element) !== element) {
+            this.parent.set(element, this.find(this.parent.get(element)));
+        }
+        return this.parent.get(element);
+    }
+
+    union(x, y) {
+        let xRoot = this.find(x);
+        let yRoot = this.find(y);
+        if (xRoot === yRoot) return;
+
+        if (this.rank.get(xRoot) < this.rank.get(yRoot)) {
+            this.parent.set(xRoot, yRoot);
+        } else if (this.rank.get(xRoot) > this.rank.get(yRoot)) {
+            this.parent.set(yRoot, xRoot);
+        } else {
+            this.parent.set(yRoot, xRoot);
+            this.rank.set(xRoot, this.rank.get(xRoot) + 1);
+        }
     }
 }
