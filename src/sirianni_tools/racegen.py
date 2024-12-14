4259e8a3aa04edee04dc13dc2a2db46c89dbcc37
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-12-14 18:41:18 +0100

    Generate race config like Sirianni via Python

--- src/sirianni_tools/racegen.py
+++ src/sirianni_tools/racegen.py
@@ -1,57 +1,34 @@
-"""Generate a Torcs race setup in quickrace.xml."""
+"""Generate TORCS race setup for simulation and track export."""
 
 __author__ = "Jacopo Sirianni"
 __copyright__ = "Copyright 2015-2016, Jacopo Sirianni"
 __license__ = "GPL"
 __email__ = "jacopo.sirianni@mail.polimi.it"
 
+import os
 
+# Fixed bot configuration - these are always used for racing
+RACING_BOTS = [
+    ("simplix_V1_10", "0"),
+    ("bt", "1"),
+    ("tita", "4"),
+    ("olethros", "2"),
+    ("damned", "3")
+]
 
-defaultBotList = []
-defaultBotList.append(("bt", "1"))
-defaultBotList.append(("tita", "4"))
-defaultBotList.append(("damned", "3"))
-defaultBotList.append(("olethros", "2"))
-defaultBotList.append(("mluc_blueeagle", "0"))
-defaultBotList.append(("simplix_trb1a", "0"))
-defaultBotList.append(("wdbee_robotics", "0"))
-defaultBotList.append(("mluc_2014", "0"))
+# Track exporter bot
+TRACK_EXPORTER = ("trackexporter", "1")
 
-trackLogger = []
-trackLogger.append(("simplix_trb1a", "0"))
-
-trackexporterBotList = []
-trackexporterBotList.append(("trackexporter", "1"))
-
-
-
-def racegen(trackName, trackType, botList, numLaps, path):
-    # Generate drivers section
-
-    i = 0
-    driversSection = ""
-    while i < len(botList):
-        driversSection += '''
-        <section name="''' + str(i+1) + '''">
-            <attnum name="idx" val="''' + botList[i][1] + '''"/>
-            <attstr name="module" val="''' + botList[i][0] + '''"/>
-        </section>'''
-        i += 1
-
-    # Generate race file
-
-    with open(path, "w") as raceFile:
-        raceFile.write(
-'''<?xml version="1.0" encoding="UTF-8"?>
-
+def generate_track_export_xml(path):
+    """Generate XML for track export (1 lap, trackexporter only)"""
+    xml = '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE params SYSTEM "../libs/tgf/params.dtd">
 
 <params name="Quick Race" type="param" mode="mw">
-
     <section name="Tracks">
         <section name="1">
-            <attstr name="name" val="''' + trackName + '''"/>
-            <attstr name="category" val="''' + trackType + '''"/>
+            <attstr name="name" val="output"/>
+            <attstr name="category" val="road"/>
         </section>
     </section>
 
@@ -62,13 +39,59 @@ def racegen(trackName, trackType, botList, numLaps, path):
     </section>
 
     <section name="Quick Race">
-        <attnum name="laps" val="''' + str(numLaps) + '''"/>
+        <attnum name="laps" val="1"/>
         <attstr name="type" val="race"/>
         <attstr name="starting order" val="random"/>
     </section>
 
-    <section name="Drivers">'''
-    + driversSection + '''
+    <section name="Drivers">
+        <section name="1">
+            <attnum name="idx" val="1"/>
+            <attstr name="module" val="trackexporter"/>
+        </section>
+    </section>
+</params>'''
+    
+    with open(path, "w") as f:
+        f.write(xml)
+
+def generate_race_xml(path, num_laps=10):
+    """Generate XML for race simulation with all standard bots"""
+    # Generate drivers section with all racing bots
+    drivers_section = ""
+    for i, (bot_name, bot_idx) in enumerate(RACING_BOTS, 1):
+        drivers_section += f'''
+        <section name="{i}">
+            <attnum name="idx" val="{bot_idx}"/>
+            <attstr name="module" val="{bot_name}"/>
+        </section>'''
+
+    xml = f'''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE params SYSTEM "../libs/tgf/params.dtd">
+
+<params name="Quick Race" type="param" mode="mw">
+    <section name="Tracks">
+        <section name="1">
+            <attstr name="name" val="output"/>
+            <attstr name="category" val="road"/>
+        </section>
     </section>
 
-</params>''')
+    <section name="Races">
+        <section name="1">
+            <attstr name="name" val="Quick Race"/>
+        </section>
+    </section>
+
+    <section name="Quick Race">
+        <attnum name="laps" val="{num_laps}"/>
+        <attstr name="type" val="race"/>
+        <attstr name="starting order" val="random"/>
+    </section>
+
+    <section name="Drivers">{drivers_section}
+    </section>
+</params>'''
+    
+    with open(path, "w") as f:
+        f.write(xml)
