ce5a1d02049a1dc0acb84cab5b34f7b48e7f99de
Martino <72280379+martinopiaggi@users.noreply.github.com>
2024-12-28 22:54:00 +0100

    Removed simplix logger (not necessary)

--- src/torcs-1.3.7-master/src/drivers/simplix-logger/simplix.mk
+++
@@ -1,7 +0,0 @@
-.PHONY: all
-all:
-	@echo Executing Pre Build commands ...
-	@export TORCS_BASE=/home/daniele/research/ongoing/championship2009/torcs-1.3.1/
-	@export MAKE_DEFAULT=/home/daniele/research/ongoing/championship2009/torcs-1.3.1/Make-default.mk
-	@echo Done
-	@make -f "Makefile" install
